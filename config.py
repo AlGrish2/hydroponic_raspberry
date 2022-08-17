1ad124046091a9cb91bc05eb6a51ca52ec407449
Idyatov Artur <iar@napoleonit.ru>
2022-08-17 15:49:07 +0000

    uploading to s3 func

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
+import os
+
+
 video_duration = 60
 fps = 10
 x_start = 238
 x_end = 538
 confidence_treshold = 0.5
+
+# s3 config
+videos_bucket: str = os.environ.get('VIDEOS_BUCKET', None)
+processed_videos_bucket: str = os.environ.get('PROCESSED_VIDEOS_BUCKET', None)

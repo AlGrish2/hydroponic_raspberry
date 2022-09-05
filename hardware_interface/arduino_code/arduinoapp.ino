655c26e78d25b7543a31cd6dc545790376b1014e
Alexey Grishakov <alexeygrish5@gmail.com>
2022-09-06 02:10:16 +0300

    Arduino controls stepper, rpi gets sensors

--- hardware_interface/arduino_code/arduinoapp.ino
+++ hardware_interface/arduino_code/arduinoapp.ino
@@ -1,9 +1,15 @@
+#include <Adafruit_ADS1X15.h>
 #include <BH1750.h>                 //light sensor library
 #include <BME280I2C.h>              //atmospheric sensor library
 #include <Rtc_Pcf8563.h>            //RTC library
 #include <iarduino_I2C_pH.h>        //pH sensor library
 #include <iarduino_I2C_TDS.h>       //TDS sensor library
 #include <Wire.h>                   //I2C library
+#include <SD.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+
 BH1750 bh1750_a;                    //begin light sensor library for sensor 1
 BH1750 bh1750_b;                    //begin light sensor library for sensor 2
 BH1750 lightMeter;                  //begin light sensor library
@@ -11,6 +17,34 @@ BME280I2C bme;                      //begin atmospheric sensor library
 Rtc_Pcf8563 rtc;                    //begin RTC library
 iarduino_I2C_pH sensor(0x19);       //begin pH sensor library + declare pH sensor address
 iarduino_I2C_TDS tds(0x29);         //begin TDS sensor library + declare EC sensor address
+Adafruit_ADS1115 ads;
+File myFile;
+OneWire oneWire(A2);
+DallasTemperature sensors(&oneWire);
+
+#define pump_pin 4;                   //pump pin, in this case, corresponds to EK 2
+#define pumprate 100;               //milliliters per hour for persithaltic pump at 12 v
+#define apump_pin 2;                  //digital pin number of solution A pump
+#define bpump_pin 5;                  //digital pin nubmer of solution B pump
+float light_level_a;                  //variable for light sensor 1
+float light_level_b;                  //variable for light sensor 2
+int hour = 0;                         //variable for hours in RTC
+int minute = 0;                       //variable for minutes in RTC
+int sec = 0;                          //variable for seconds in RTC
+float temp(NAN), hum(NAN), pres(NAN); //variables for air conditions sensor
+float pH = 0;                         //variable for pH
+float EC = 0;                         //variable for Electrical conductivity
+float TDS = 0;                        //variable for total dissolved solids
+bool wlupper;                      //variable for upper water level sensor
+bool wllower;                      //variable for lower water level sensor
+float temperature = 22.5;             //variable for temperature
+int failcount = 0;
+//bool mpump = 0;                       //main pump state variable
+//float aprt = 0;                       //pump a run time variable
+//float bprt = 0;                       //pump b run time variable
+//float avolume = 0;                    //volume of solution A needed to be added
+//float bvolume = 0;                    //volume of solution B needed to be added
+
 
 void setup() 
 {
@@ -32,58 +66,14 @@ void setup()
   }
   rtc.initClock();                                                    //begin RTC operation
   rtc.setDate(17, 7, 7, 20, 22);                                      //Set date to be done through RPi SPI connection(day, weekday, month, century, year
-  pinMode(3, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(4, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(3, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(4, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(3, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(4, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(3, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(4, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(3, INPUT);                                                  //Declare pin mode for pin 
-  pinMode(4, INPUT);                                                  //Declare pin mode for pin 
-}
-float light_level_a;                  //variable for light sensor 1
-float light_level_b;                  //variable for light sensor 2
-int hour = 0;                         //variable for hours in RTC
-int minute = 0;                       //variable for minutes in RTC
-int sec = 0;                          //variable for seconds in RTC
-float temp(NAN), hum(NAN), pres(NAN); //variables for air conditions sensor
-float pH = 0;                         //variable for pH
-float EC = 0;                         //variable for Electrical conductivity
-float TDS = 0;                        //variable for total dissolved solids
-bool wlupper = false;                 //variable for upper water level sensor
-bool wllower = false;                 //variable for lower water level sensor
-float temperature = 22.5;             //variable for temperature
-bool mpump = 0;                       //main pump state variable
-const int pump_pin = 4;               //pump pin, in this case, corresponds to EK 2
-float aprt = 0;                       //pump a run time variable
-float bprt = 0;                       //pump b run time variable
-float avolume = 0;                    //volume of solution A needed to be added
-float bvolume = 0;                    //volume of solution B needed to be added
-const int pumprate = 100;             //milliliters per hour for persithaltic pump at 12 v
-const int apump_pin = 2;              //digital pin number of solution A pump
-const int bpump_pin = 2;              //digital pin nubmer of solution B pump
-
-void loop()                                           //Main code
-{
-  light_sensors_data();
-  print_light_sensor();
-  BME280data();
-  print_air_sensor(&Serial);
-//  input_time_serial();
-  print_time();
-  pH_data();
-  print_pH();
-  ec_data();
-  print_ec();
-  water_level_sense();
-  print_wl();
-  Serial.print("\n\r");
-  delay(1000);
+  ads.begin();
+  sensors.begin();
 }
 
-void light_sensors_data ()                          //update light sensor variables to most recent reading
+
+
+
+void light_sensors_data()                          //update light sensor variables to most recent reading
 {
   if (bh1750_a.measurementReady()) {
     light_level_a = bh1750_a.readLightLevel();
@@ -93,14 +83,10 @@ void light_sensors_data ()                          //update light sensor variab
   }
 }
 
-void print_light_sensor()                           //serial print light sensor readings
+void wtemperature_data()
 {
-  
-  Serial.print("Light1 :");
-  Serial.print(light_level_a);
-  Serial.print("\t\tLight2 :");
-  Serial.print(light_level_b);
-  Serial.print("\n\r");
+  sensors.requestTemperatures();
+  temperature = sensors.getTempCByIndex(0);
 }
 
 void BME280data()                                   //update atmospheric sensor variables to most recent reading
@@ -111,59 +97,10 @@ void BME280data()                                   //update atmospheric sensor 
   bme.read(pres, temp, hum, tempUnit, presUnit);
 }
 
-void print_air_sensor(Stream* client)                //serial print light sensor readings
-{
-  BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
-  BME280::PresUnit presUnit(BME280::PresUnit_Pa);
-
-  bme.read(pres, temp, hum, tempUnit, presUnit);
-  
-  client->print("Temp: ");
-  client->print(temp);
-  client->print("°C");
-  client->print("\t\tHumidity: ");
-  client->print(hum);
-  client->print("% RH");
-  client->print("\t\tPressure: ");
-  client->print(pres);
-  client->print("Pa");
-  client->print("\n\r");
-}
-
-void rtc_get_time()                                 //update second, minute and hour variables to most recent RTC readings
-{
-  rtc.getTime();
-}
-void print_time()                                   //serial print time
-{
-  Serial.print("Time: ");
-  Serial.print(rtc.formatTime());
-  Serial.print("\n\r");
-}
-void input_time_serial()                            //work in progress function for reading time from serial input
-{
-  if(Serial.available() > 0) // function for getting variables for time
-  {
-    Serial.print("input hour");
-    hour = Serial.read();
-    Serial.print("input minute");
-    minute = Serial.read();
-    Serial.print("input sec");
-    sec = Serial.read();
-  }
-  rtc.setTime(hour, minute, sec); // sets time on RTC
-}
 
 void pH_data()                                      //update pH sensor variable to most recent reading
 {
    pH = sensor.getPH();
-}
-
-void print_pH()                                     //serial print pH value
-{
-  Serial.print("pH: ");
-  Serial.print(pH);
-  Serial.print("\t\t");
 }
 
 void ec_data()                                      //update TDS sensor variables to most recent reading
@@ -172,70 +109,64 @@ void ec_data()                                      //update TDS sensor variable
   EC = tds.get_S();
   TDS = tds.getTDS();
 }
-void print_ec()                                     //serial print TDS sensor readings
-{
-  Serial.print("EC: ");
-  Serial.print(tds.get_S());
-  Serial.print("mS/cm" );
-  Serial.print("\t\tTDS: ");
-  Serial.print(tds.getTDS());
-  Serial.print("mg/L\n\r");
-}
 
 void water_level_sense()                            //update water level sensor variables to most recent reading
 {
-  if (digitalRead(3) == HIGH) {wlupper = true;}
-  else {wlupper = false;}
-  if (digitalRead(4) == HIGH) {wllower = true;}
-  else {wllower = false;}
+  if (ads.readADC_SingleEnded(0) < 14000) {wlupper = false;}
+  else {wlupper = true;}
+  if (ads.readADC_SingleEnded(1) < 1400) {wllower = false;}
+  else {wllower = true;}
 }
 
-void print_wl()                                     //serial print water level readings
+void record_sd()                                     //spi communication protocol
 {
-  Serial.print("Upper Sensor: ");
-  Serial.print(wlupper);
-  Serial.print("\t\tLower Sensor: ");
-  Serial.print(wllower);
-  Serial.print("\n\r");
-}
-
-void SPI_COMM()                                     //spi communication protocol
-{
-  
-}
-
-void pump_main_switch()                             //change main pump state(on -> off or off -> on)
-{
-  if(mpump == 1)
+  if (!SD.begin(10)) {while (1);}
+  myFile = SD.open("records.txt", FILE_WRITE);
+  if (myFile) 
   {
-    digitalWrite(pump_pin, LOW);
-    Serial.print("pump off\n\r");
-    mpump = 0;
+    myFile.println(rtc.formatDate());
+    myFile.println(", ");
+    myFile.println(rtc.formatTime());
+    myFile.println(", ");
+    myFile.println(temp);
+    myFile.println(", ");
+    myFile.println(hum);
+    myFile.println(", ");
+    myFile.println(pres);
+    myFile.println(", ");
+    myFile.println(temperature);
+    myFile.println(", ");
+    myFile.println(light_level_a);
+    myFile.println(", ");
+    myFile.println(light_level_b);
+    myFile.println(", ");
+    myFile.println(EC);
+    myFile.println(", ");
+    myFile.println(TDS);
+    myFile.println(", ");
+    myFile.println(wllower);
+    myFile.println(", ");
+    myFile.println(wlupper);
+    myFile.close();
   }
   else
   {
-    digitalWrite(pump_pin, HIGH);
-    Serial.print("pump on\n\r");
-    mpump = 1;
+    failcount++;
   }
 }
 
-void apump_run()                                  //run pump for solution A to add a set volume of solution a
+void loop()                                           //Main code
 {
-  aprt = avolume/pumprate;
-  while(sec+60*minute+3600*hour < sec+60*minute+3600*hour+aprt)
+  rtc.getMinute();
+  if(minute <= 3)
   {
-    digitalWrite(apump_pin, HIGH);
+    light_sensors_data();
+    BME280data();
+    ec_data();
+    water_level_sense();
+    wtemperature_data();
+    record_sd();
+    delay(180000);
   }
-  digitalWrite(apump_pin, LOW);
-}
-
-void bpump_run()                                  //run pump for solution B to add a set volume of solution a
-{
-  bprt = bvolume/pumprate;
-  while(sec+60*minute+3600*hour < sec+60*minute+3600*hour+bprt)
-  {
-    digitalWrite(bpump_pin, HIGH);
-  }
-  digitalWrite(bpump_pin, LOW);
+  else {delay(180000);}
 }
